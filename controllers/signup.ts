8857c54d179ef47ce4038e59966cd46a4f12ab1a
izroth <lakhansharma889027@gmail.com>
2023-08-21 09:52:58 +0530

    added one more condintion

--- controllers/signup.ts
+++ controllers/signup.ts
@@ -6,40 +6,53 @@ dotenv.config();
 const saltRounds = 10;
 
 const signup = (req: any, res: any) => {
-    try{
+    try {
         const { name, email, password } = req.body;
         if (!name || !email || !password) {
             return res.status(400).json('Incorrect form submission');
         }
-        const saltRounds = 10;
-        bcrypt.hash(password, saltRounds, (err: any, hash: any) => {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                singupdb.query(
-                    `INSERT INTO signup (name, email, password, timestamp) VALUES (?,?,?,NOW())`,
-                    [name, email, hash],
-                    (err: any, results: any) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            console.log('Data inserted into signup table');
-                        }
+        // Check if user already exists
+        singupdb.query(
+            `SELECT * FROM signup WHERE email = ?`,
+            [email],
+            (err: any, results: any) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json('Database error');
+                } else {
+                    if (results.length > 0) {
+                        return res.status(400).json('User already exists');
+                    } else {
+                        // Hash password
+                        bcrypt.hash(password, saltRounds, (hashErr: any, hash: any) => {
+                            if (hashErr) {
+                                console.log(hashErr);
+                                return res.status(500).json('Hashing error');
+                            } else {
+                                // Insert data into the signup table
+                                singupdb.query(
+                                    `INSERT INTO signup (name, email, password, timestamp) VALUES (?,?,?,NOW())`,
+                                    [name, email, hash],
+                                    (insertErr: any, insertResults: any) => {
+                                        if (insertErr) {
+                                            console.log(insertErr);
+                                            return res.status(500).json('Database error');
+                                        } else {
+                                            console.log('Data inserted into signup table');
+                                            return res.status(200).json('Success');
+                                        }
+                                    }
+                                );
+                            }
+                        });
                     }
-                );
+                }
             }
-        }
         );
-        res.status(200).json('success');
-
-
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json('An error occurred');
     }
-    catch(err){
-        console.log(err)
-        res.status(400).message('An error occured')
-    }
-}
+};
+
 module.exports = signup;
-
