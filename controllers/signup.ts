b0ed00ec462bbcd9d6bf3d3058bc4dd6a05702f0
izroth <lakhansharma889027@gmail.com>
2023-08-25 10:56:55 +0530

    added discrod ping after singup

--- controllers/signup.ts
+++ controllers/signup.ts
@@ -2,6 +2,7 @@ const singupdb = require('../db/db.ts');
 const bcrypt = require('bcrypt');
 const jsonwebtokenlogin = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const { default: axios } = require('axios');
 dotenv.config();
 const saltRounds = 10;
 
@@ -57,10 +58,27 @@ const signup = async (req: any, res: any) => {
                         console.log(err);
                         return reject('Database error');
                     }
+                    
+        //ping the user data to the discord server
+    
+        axios.post( process.env.DISCORD_WEBHOOK_URL, {
+            content: `New user signed up: ${name} ${email}`
+        })
+            .then((res: any) => {
+                console.log(`statusCode: ${res.statusCode}`)
+                console.log(res)
+            })
+            .catch((error: any) => {
+                console.error(error)
+            })
+            
                     resolve(results);
                 }
             );
         });
+       
+
+        
 
         console.log('Data inserted into signup table');
         return res.status(200).json('Success');
